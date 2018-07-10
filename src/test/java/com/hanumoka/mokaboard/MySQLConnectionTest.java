4c61058111f71943ebb61fefe8d56cad25affabc
amagramer-labtop <amagramer@gmail.com>
2018-07-11 01:27:43 +0900

    1

---
+++ src/test/java/com/hanumoka/mokaboard/MySQLConnectionTest.java
@@ -0,0 +1,27 @@
+package com.hanumoka.mokaboard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+
+public class MySQLConnectionTest {
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/mokaboard?allowPublicKeyRetrieval=true&useSSL=false";
+	private static final String USER = "hanumoka";
+	private static final String PW = "password";
+
+	@Test
+	public void testConnection() throws Exception {
+
+		Class.forName(DRIVER);
+
+		try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
+			System.out.println(con);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} // try_
+	}// testConnection_
+}
