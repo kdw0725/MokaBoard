0869aaa11d2c41bca198572b5466deef41708c6d
amagramer-labtop <amagramer@gmail.com>
2018-07-16 16:16:01 +0900

    mysql 연동및 테스트 코드 추가

--- src/test/java/com/hanumoka/mokaboard/MySQLConnectionTest.java
+++ src/test/java/com/hanumoka/mokaboard/MySQLConnectionTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 
 public class MySQLConnectionTest {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://127.0.0.1:3306/mokaboard?allowPublicKeyRetrieval=true&useSSL=false";
-	private static final String USER = "hanumoka";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/mokaboard_dev?allowPublicKeyRetrieval=true&useSSL=false";
+	private static final String USER = "mokaboard";
 	private static final String PW = "password";
 
 	@Test
