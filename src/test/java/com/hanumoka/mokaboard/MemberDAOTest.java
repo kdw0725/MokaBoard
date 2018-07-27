62c03387c0f1b552b1869fca60dd8327574c0978
amagramer-labtop <amagramer@gmail.com>
2018-07-27 18:10:39 +0900

    17: mybatis log4j 적용
    Task-Url: http://github.com/hanumoka/MokaBoard/issues/17

--- src/test/java/com/hanumoka/mokaboard/MemberDAOTest.java
+++ src/test/java/com/hanumoka/mokaboard/MemberDAOTest.java
@@ -5,34 +5,34 @@ import javax.inject.Inject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.hanumoka.mokaboard.dao.MemberDAO;
 import com.hanumoka.mokaboard.vo.MemberVO;
 
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 //mybatis dao 테스트 
-@WebAppConfiguration
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
 public class MemberDAOTest {
 	
 	@Inject
-	private MemberDAO dao;
+	private MemberDAO memberDAO;
 	
 	@Test
 	public void testTime() throws Exception{
-		System.out.println("======= :" + dao.getTime());
+		System.out.println("======= :" + memberDAO.getTime());
 	}
 	
 	@Test
 	public void testInsertMember() throws Exception{
 		MemberVO vo = new MemberVO();
-		vo.setUserid("user07");
-		vo.setUserpw("user07");
-		vo.setUsername("USER07");
-		vo.setEmail("user07@aaa.com");
+		vo.setUserid("user08");
+		vo.setUserpw("user08");
+		vo.setUsername("USER08");
+		vo.setEmail("user08@aaa.com");
 		
-		dao.insertMember(vo);
+		memberDAO.insertMember(vo);
 	}
 }
