a400c1446bd8a8e830164cd8f5ec4e8f10ff40d9
amagramer-labtop <amagramer@gmail.com>
2018-08-04 05:42:42 +0900

    18: 자유게시판 게시물 등록 수정 삭제 조회
    Task-Url: http://github.com/hanumoka/MokaBoard/issues/18
    
    BOARDLIST 화면 디자인 완성
    JS, CSS 다시 정리

--- src/main/java/com/hanumoka/sample/controller/SampleBoardController.java
+++ src/main/java/com/hanumoka/sample/controller/SampleBoardController.java
@@ -2,6 +2,8 @@ package com.hanumoka.sample.controller;
 
 import java.util.Locale;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,31 +11,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.hanumoka.sample.service.BoardService;
+import com.hanumoka.sample.vo.BoardVO;
+
 
 @Controller
+@RequestMapping("/samplehome/board/*")
 public class SampleBoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SampleBoardController.class);
 	
-	@RequestMapping(value = "/samplehome/board/list", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String registerGET(BoardVO board, Model model) {
+		logger.info("register get......");
+		return "/samples/board/register";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO board, Model model) throws Exception {
+		logger.info("register post......");
+		logger.info(board.toString());
+		
+		service.regist(board);
+		
+		model.addAttribute("result", "success");
+		
+		//return "/samples/board/success";
+		return "redirect:/samplehome/board/list";
+	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Locale locale, Model model) {
 		return "/samples/board/list";
 	}
 	
-	@RequestMapping(value = "/samplehome/board/mod", method = RequestMethod.GET)
+	@RequestMapping(value = "/mod", method = RequestMethod.GET)
 	public String mod(Locale locale, Model model) {
 		return "/samples/board/mod";
 	}
 	
-	@RequestMapping(value = "/samplehome/board/read", method = RequestMethod.GET)
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
 	public String read(Locale locale, Model model) {
 		return "/samples/board/read";
 	}
 	
-	@RequestMapping(value = "/samplehome/board/register", method = RequestMethod.GET)
-	public String register(Locale locale, Model model) {
-		return "/samples/board/register";
-	}
+	
 	
 	
 }
