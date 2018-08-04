59264a4ce2144850fea04b87198ed2b920f52d71
amagramer-labtop <amagramer@gmail.com>
2018-08-04 17:19:55 +0900

    18: 자유게시판 게시물 등록 수정 삭제 조회
    Task-Url: http://github.com/hanumoka/MokaBoard/issues/18
    
    listAll 완료
    
    단순 전체 조회.... 부트스트랩으로 자동 페이징이 되긴함(서버처리 안함)

--- src/main/java/com/hanumoka/sample/controller/SampleBoardController.java
+++ src/main/java/com/hanumoka/sample/controller/SampleBoardController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hanumoka.sample.service.BoardService;
 import com.hanumoka.sample.vo.BoardVO;
@@ -31,20 +32,27 @@ public class SampleBoardController {
 	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registerPOST(BoardVO board, Model model) throws Exception {
+	public String registerPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
 		logger.info("register post......");
 		logger.info(board.toString());
 		
 		service.regist(board);
 		
-		model.addAttribute("result", "success");
+		//model.addAttribute("result", "success");
+		
+		rttr.addFlashAttribute("msg", "success");
 		
 		//return "/samples/board/success";
 		return "redirect:/samplehome/board/list";
 	}
 	
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public String list(Locale locale, Model model) {
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public String listAll(Locale locale, Model model) throws Exception {
+		
+		logger.info("show all list........");
+		
+		model.addAttribute("list", service.listAll());
+		
 		return "/samples/board/list";
 	}
 	
